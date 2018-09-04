388c757ba5edbbd5852e3548a917875ca8fde373
Sandeep Balivada <sandeepbalivada123@gmail.com>
2018-09-04 16:09:00 +0530

    wipro 2nd problem commit

---
+++ wipro2/main.cpp
@@ -0,0 +1,39 @@
+/*
+print pattern:
+n=4
+
+1
+3*2
+6*5*4
+10*9*8*7
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter the number of lines of patterns you wish to see:" << endl;
+    int n,val=1,fin,flag=0;
+    cin>>n;
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=i;k++)
+        {
+         fin=val-1+i;
+         cout<<fin-2*flag;
+         flag++;
+         if(k<i)
+            cout<<"*";
+         else
+            flag=0;
+         val++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
